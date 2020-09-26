b938ece7e1a72adb78bfe00df6e81c30e37ecdc1
mhtrimech <mhtrimech@solocal.com>
2020-09-27 00:38:11 +0200

    chapter 13 : 216. Recreating the Deployment

---
+++ k8s-complex/client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+              <h2>Fib Calculation</h2>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div className="App-body">
+            <div>
+              <Route exact path="/" component={Fib}/>
+              <Route exact path="/otherpage" component={OtherPage}/>
+            </div>
+          </div>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
